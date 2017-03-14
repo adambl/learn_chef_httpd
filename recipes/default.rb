76bb0987526bdd4494abe1905fa902e381b7253e
adambl <adamblaiberg@gmail.com>
2017-03-14 09:35:07 +0000

    chef cookbook to deploy and start apache

---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: learn_chef_httpd
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'apache2'
+
+service 'apache2' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
